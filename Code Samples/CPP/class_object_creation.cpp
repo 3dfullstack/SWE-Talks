29393bb33bbcffc5e8e3c7564d75d0f9fbcc4585
codersguild <shekharhiran@hotmail.com>
2020-10-02 21:33:09 +0530

    Update

--- Code Samples/CPP/class_object_creation.cpp
+++ Code Samples/CPP/class_object_creation.cpp
@@ -1,18 +1,24 @@
- #include <iostream>
- 
- class Test {
-	 static int x;
-	 public:
-	  	Test(){x++; std::cout << "Constructor called \n";}
-		static int getX(){return x;}
+#include <iostream>
 
- };
- int Test::x = 0;
- int main(){
-	 Test t[2]; // With array kind of declaration,no. of objects are made.
-	 // here 2 objects are made. 
-	
-	 
-	 std::cout << t[23333].getX(); ;
- }
- // what is the meaing ofusing [] for class object creation.
+class Test
+{
+	static int x;
+
+public:
+	Test()
+	{
+		x++;
+		std::cout << "Constructor called \n";
+	}
+	static int getX() { return x; }
+};
+int Test::x = 0;
+int main()
+{
+	Test t[2]; // With array kind of declaration,no. of objects are made.
+	// here 2 objects are made.
+
+	std::cout << t[23333].getX();
+	;
+}
+// what is the meaing ofusing [] for class object creation.
