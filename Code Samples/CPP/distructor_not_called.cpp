de2bf2e9295b40f1427574f24b47e5f5bec430a4
sandy1618 <sandeepnayak1.618@gmail.com>
2020-10-07 21:54:15 +0900

    Added new files, but also moved files from code samples

--- Code Samples/CPP/distructor_not_called.cpp
+++ Code Samples/CPP/distructor_not_called.cpp
@@ -11,12 +11,20 @@ public :
     }
 };
 int main()
-{
-   vector <a*> *v1  = new vector<a*>;
-   vector <a> *v2  = new vector<a>;
-   delete v1;
-   delete v2;
+{   
+   vector <a*> *v1  = new vector<a*>(); // v1 is a pointer. 
+   vector <a> *v2  = new vector<a>();// a new vecotr of type a is created/
+    
+//    delete v1;// did not call anything/.  
+//    delete v2;
+
+   a *b = new a();
+   delete b;
+
    return 0;
 }
 
-// Why distructor not called?
+// Why distructor not called? because : a new vector which stores a is created. 
+// not a is created. 
+// Further, allocators are the main garbage collectors for vectors. 
+// so delete has no effect on vectors. 
