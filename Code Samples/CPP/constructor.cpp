2b10db9830ed01eead878ef8c0267cd94d773e21
Sumit Lahiri <shekharhiran@hotmail.com>
2020-10-07 21:54:24 +0530

    Revert "Refactoring Code Structure and Git PR Stuff (#5)"
    This reverts commit 6f0ec0d9501474a830add03a72e4bf8e1a903fbb.

--- Code Samples/CPP/constructor.cpp
+++ Code Samples/CPP/constructor.cpp
@@ -16,12 +16,10 @@ public:
 Point::Point(int i = 0, int j = 0)
 {
 	// Doubt: When both i = 0 and j = 0, it act like Point()? => No. Refer discussion.
-	// Clang Output : error: addition of default argument on redeclaration makes this constructor a default constructor
 	x = i;
 	y = j;
 	cout << "Parameterized constructor called";
 }
-
 Point::Point()
 {
 	cout << "Default constructor called";
@@ -30,7 +28,6 @@ Point::Point()
 int main()
 {
 	// No ambiguity in choosing the right constructor here.
-	// warning: empty parentheses interpreted as a function declaration [-Wvexing-parse]
 	Point t1(), *t2;
 	return 0;
 }
